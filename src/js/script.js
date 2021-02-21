32a7b3817259c0cc8e8f73e0da9473b4a13c27e3
AlexS <dtmakus@gmail.co>
2021-02-21 23:31:34 +0600

    validate

--- src/js/script.js
+++ src/js/script.js
@@ -18,12 +18,16 @@ $(document).ready(function(){
     }
   ]
 });
+
+//Tabs
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
   
+  //More
+
   function toggleSlide(item){
       $(item).each(function(i){
       $(this).on('click', function(e){
@@ -35,5 +39,96 @@ $(document).ready(function(){
   };
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  //Modal
+
+  $('[data-modal=consultation]').on('click', function(){
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation,#thanks ,#order').fadeOut('slow');
+  })
+  $('.button_itm').on('click', function(){
+    $('.overlay, #order').fadeIn();
+  });
+  $('.button_itm').each(function(i){
+    $(this).on('click', function(){
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+    })
+  });
+
+  $('.feed-form').validate({
+    name: "required",
+    phone: "required",
+    email: {
+      required: true,
+      email: true
+    },
+    messages:{
+      name: "Пожалуйста введите свое имя",
+      phone:"Пожалуйста введите свой номер телефона",
+      email:{
+        required: "Пожалуйста введите свой email",
+        email:"Провеьте правильность написания email"
+      }
+    }
+  });
+  $('#consultation form').validate({
+    name: "required",
+    phone: "required",
+    email: {
+      required: true,
+      email: true
+    },
+    messages:{
+      name: "Пожалуйста введите свое имя",
+      phone:"Пожалуйста введите свой номер телефона",
+      email:{
+        required: "Пожалуйста введите свой email",
+        email:"Провеьте правильность написания email"
+      }
+    }
+  });
+  $('#order form').validate({
+    name: "required",
+    phone: "required",
+    email: {
+      required: true,
+      email: true
+    },
+    messages:{
+      name: "Пожалуйста введите свое имя",
+      phone:"Пожалуйста введите свой номер телефона",
+      email:{
+        required: "Пожалуйста введите свой email",
+        email:"Провеьте правильность написания email"
+      }
+    }
+  });
+  //Validation
+  $('form').validate();
+  function validateForms(form){
+    $('form').validate({
+    name: "required",
+    phone: "required",
+    email: {
+      required: true,
+      email: true
+    },
+    messages:{
+      name: "Пожалуйста введите свое имя",
+      phone:"Пожалуйста введите свой номер телефона",
+      email:{
+        required: "Пожалуйста введите свой email",
+        email:"Провеьте правильность написания email"
+      }
+    }
+  });
+};
+
+  validateForms('#order form');
+  validateForms('#consultation form');
+  validateForms('#consultation-form');
+    
 });
   
